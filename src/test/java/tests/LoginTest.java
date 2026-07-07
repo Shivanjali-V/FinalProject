e30a04a53d14e9f1ee6134f75009d27d1f5ab9f4
Sharan <2497726@cognizant.com>
2026-07-07 18:21:29 +0530

    implemented the login test, new Bike test and Excel utilities

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,48 @@
 package tests;
 
-public class LoginTest {
+import baseTest.DriverSetupTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utilities.ScreenShotUtil;
+
+import java.io.IOException;
+
+public class LoginTest extends DriverSetupTest {
+
+    @Test()
+    public void clickLoginIcon(){
+        LoginPage lt = new LoginPage(driver);
+        lt.clickLoginIcon();
+    }
+
+    @Test(dependsOnMethods = "clickLoginIcon")
+    public void clickGoogleBtn() throws InterruptedException {
+        LoginPage lt = new LoginPage(driver);
+        lt.clickGoogleBtn();
+    }
+
+    @Test(dependsOnMethods = "clickGoogleBtn")
+    public void switchToGoogleWindow() throws InterruptedException {
+        LoginPage lt = new LoginPage(driver);
+        lt.switchToGoogleWindow();
+    }
+
+    @Test(dependsOnMethods = "switchToGoogleWindow")
+    public void checkWindowHeading(){
+        LoginPage lt = new LoginPage(driver);
+        lt.checkWindowHeading();
+    }
+
+    @Test(dependsOnMethods = "checkWindowHeading")
+    public void clickNextBtn(){
+        LoginPage lt = new LoginPage(driver);
+        lt.clickNextBtn();
+    }
+
+    @Test(dependsOnMethods = "clickNextBtn")
+    public void takeScreenShot() throws IOException {
+        ScreenShotUtil sc = new ScreenShotUtil(driver);
+        sc.takeScreenShot();
+    }
+
 }
--- src/test/java/tests/NewBikeTest.java
+++ src/test/java/tests/NewBikeTest.java
@@ -1,4 +1,45 @@
 package tests;
 
-public class NewBikeTest {
+import baseTest.DriverSetupTest;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.NewBikesPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewBikeTest extends DriverSetupTest {
+    List<WebElement> bikeList = new ArrayList<>();
+
+    @Test(priority = 0)
+    public void clickUpComingBikes(){
+        HomePage hp = new HomePage(driver);
+        hp.navigateToUpcomingBikes();
+    }
+
+    @Test(dependsOnMethods = "clickUpComingBikes")
+    public void clickBrandName() throws InterruptedException {
+        NewBikesPage nbp = new NewBikesPage(driver);
+        nbp.clickBrandName();
+    }
+
+    @Test(dependsOnMethods = "clickBrandName")
+    public void selectFilter() throws InterruptedException {
+        NewBikesPage nbp = new NewBikesPage(driver);
+        nbp.selectUnder5Lakhs();
+    }
+
+    @Test(dependsOnMethods = "selectFilter")
+    public void getBikeListUnder5Lakhs(){
+        NewBikesPage nbp = new NewBikesPage(driver);
+        bikeList = nbp.getBikeList();
+    }
+
+    @Test(dependsOnMethods = "getBikeListUnder5Lakhs")
+    public void printBikeList(){
+        NewBikesPage nbp = new NewBikesPage(driver);
+        nbp.printBikeDetails(bikeList);
+    }
+
 }
