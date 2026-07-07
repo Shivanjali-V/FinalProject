b1d05974c061a20a73c772914ae1101a0dac4c82
Shivanjali-V <shivanjaliv18@gmail.com>
2026-07-07 16:35:15 +0530

    Added Property Files

--- src/test/java/baseTest/DriverSetupTest.java
+++ src/test/java/baseTest/DriverSetupTest.java
@@ -1,4 +1,21 @@
 package baseTest;
 
-public class DriverSetupTest {
+import base.DriverSetup;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public class DriverSetupTest extends DriverSetup{
+    public WebDriver driver;
+    @Parameters("browser")
+    @BeforeClass
+    public void setup(String browser) {
+        driver = setUp(browser);
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+
+        tearDown();
+
+    }
 }
