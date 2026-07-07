2a6b1b3a164e0123afd09a33547283d0b32451a7
R <2497742@cognizant.com>
2026-07-07 17:15:32 +0530

    Added UsedCarsTest.java

--- src/test/java/tests/UsedCarsTest.java
+++ src/test/java/tests/UsedCarsTest.java
@@ -1,4 +1,43 @@
 package tests;
 
-public class UsedCarsTest {
+import baseTest.DriverSetupTest;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pages.UsedCarsPage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsedCarsTest extends DriverSetupTest {
+    List<WebElement> carList = new ArrayList<WebElement>();
+
+    @Test()
+    public void navToUsedCarsPage(){
+        UsedCarsPage ucp = new UsedCarsPage(driver);
+        ucp.navigateToUsedCars();
+    }
+
+    @Test(dependsOnMethods = "navToUsedCarsPage")
+    public void selectLocation() throws InterruptedException {
+        UsedCarsPage ucp = new UsedCarsPage(driver);
+        ucp.selectLocation();
+    }
+
+    @Test(dependsOnMethods = "selectLocation")
+    public void scrollToPopularModels() throws InterruptedException {
+        UsedCarsPage ucp = new UsedCarsPage(driver);
+        ucp.scrollToPopularModels();
+    }
+
+    @Test(dependsOnMethods = "scrollToPopularModels")
+    public void selectAllModels() throws InterruptedException {
+        UsedCarsPage ucp = new UsedCarsPage(driver);
+        ucp.selectAllOptions();
+    }
+
+    @Test(dependsOnMethods = "selectAllModels")
+    public void getCarList() throws InterruptedException {
+        UsedCarsPage ucp = new UsedCarsPage(driver);
+        carList = ucp.getCarList();
+    }
+
 }
